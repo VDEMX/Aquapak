779794f229ba5a80d8a6b4e5e34965072b6483d2
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-09-30 11:26:30 -0500

    Quitar el comentario de prueba

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $active_page="Inicio"; ?>
 <?php include("head.php"); ?>
- 		<title><?php echo "$sitio"?> prueba desde coda</title>
+ 		<title><?php echo "$sitio"?></title>
  		<meta name="description" content="<?php echo "$nombre"?> Empresa fundada en 1992, dedicada a diseñar, producir y distribuir al mayoreo equipos de alta calidad con tecnología de punta para el manejo de agua.">
  		<meta name="keywords" content="<?php echo "$nombre"?>, bombas para agua, equipos de bombeo, presurizadores, equipos hidroneumaticos, tanques, motores sumergibles, bombas sumergibles para pozo, bombas aquapak, bombas para achique, bombas para lodos, bombas trituradoras, tanques aquapak, sistemas de bombeo, bombas sumergibles para aguas residuales, bombas para albercas, bombas de uso domestico, bombas para hidromasaje">
  		<meta name="robots" content="Index, follow">
