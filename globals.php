cc08906f50fad74ac34805325dd1b6b840b68c36
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-11-25 17:03:07 -0600

    aplicarles los estilos generales

--- globals.php
+++ globals.php
@@ -1,5 +1,6 @@
 <?php
 $sitio = "Aqua Pak";
-$url = "http://aquapak.com.mx/";
+//$url = "http://aquapak.com.mx/";
+$url = "http://192.168.1.100:8888/";
 $nombre = "Aqua Pak";
 ?>
--- js/funciones.js
+++ js/funciones.js
@@ -5,16 +5,6 @@ function inicio ()
 	$('.flexslider').flexslider({
 		animation: "slide"
 	});
-	//botones para el iframe
-	$(".boton").click(function toggle(){
-		//quitar la clase .active al boton que la tenga:
-		$(".active").removeClass("active");
-		//agregar la clase .active al boton al que hicimos click:
-		$(this).addClass("active");
-		//esconder el mapa
-		$("#ranimg, #Table_01, #map, #productos-nuevos, #contacto, #acercade, #fotovoltaicos, #ax, #federatas").hide();
-		$("iframe").show();
-	});
 	//acordión del menú táctil
 	$('ul').accordion();
 	//formulario de contacto
@@ -24,43 +14,12 @@ function inicio ()
         responseDiv : '#respuesta',
         allBrowsers: true,  
     }); 
-    //pestañas
-   $(".tab").click(function tabs(){
-   //quitar la clase .active a la pestaña que la tenga:
-      $(".active").removeClass("active");
-   //agregar la clase .active la pestaña que hicimos click:
-   $(this).addClass("active");
-   });
-    $('.sec-1').click(
-      function()
-      {
-            window.location = '/aviso-nov-2012';
-      }
-   );
-   $('.sec-2').click(
-      function()
-      {
-            window.location = '/aviso-nov-2012#sec-2';
-      }
-   );
-   $('.sec-02').click(
-      function()
-      {
-            window.location = '/aviso-nov-2012#sec-02';
-      }
-   );
-   $('.sec-6').click(
-      function()
-      {
-            window.location = '/aviso-nov-2012#sec-6';
-      }
-   );
-   $('.sec-8').click(
-      function()
-      {
-            window.location = '/aviso-nov-2012#sec-8';
-      }
-   );
+    //Simular el evento hover al hacer click en pantallas táctiles
+    var myLinks = document.getElementsByTagName('a');
+	for(var i = 0; i < myLinks.length; i++){
+	   myLinks[i].addEventListener('touchstart', function(){this.className = "hover";}, false);
+	   myLinks[i].addEventListener('touchend', function(){this.className = "";}, false);
+	}
 }
 
 if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))
@@ -103,9 +62,3 @@ if(("standalone" in window.navigator) && window.navigator.standalone){
 // bloquear el zoom en iOS
 MBP.scaleFix();
 }
-
-// Respond.js
-yepnope({
-	test : Modernizr.mq('(only all)'),
-	nope : ['js/respond.min.js']
-});
