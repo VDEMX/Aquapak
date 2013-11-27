2f1b6fc14366d386a5d94c994ffc2003f253cbc1
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-11-27 12:22:38 -0600

    agregar las pantallas de splash screen
    y también arregler el favicon

--- globals.php
+++ globals.php
@@ -1,6 +1,6 @@
 <?php
 $sitio = "Aqua Pak";
-//$url = "http://aquapak.com.mx/";
-$url = "http://192.168.1.100:8888/";
+$url = "http://aquapak.com.mx/";
+//$url = "http://192.168.2.140:8888/";
 $nombre = "Aqua Pak";
 ?>
